48092cc8564ff99f8ec3ab858ca7e7468db5d72c
evanmclaren <117170506+evanmclaren@users.noreply.github.com>
2023-03-20 12:18:33 +0100

    working on being able to edit student information

--- main.cpp
+++ main.cpp
@@ -7,13 +7,23 @@ public:
     int student_id;
     std::string student_name;
     std::string student_email;
-    int last_student_id = 0;
+    std::vector<student> student_list;
 
-    student(int aStudent_id, std::string aStudent_name, std::string aStudent_email){
-        student_id = last_student_id + 1;
-        last_student_id ++;
+    student(int aStudent_id, std::string aStudent_name, std::string aStudent_email) {
+        student_id = aStudent_id;
         student_name = aStudent_name;
         student_email = aStudent_email;
+    }
+
+    void modify_student(int student_index){
+        if(student_index >= 0 && student_index < student_list.size()){
+            std::cout << "Re-enter student's name: " << std::endl;
+            std::cin.ignore();
+            std::getline(std::cin, student_list[student_index].student_name);
+            std::cout << "Re-enter student's email: " << std::endl;
+            std::getline(std::cin, student_list[student_index].student_email);
+        }
+
     };
 };
 
@@ -23,7 +33,8 @@ int main() {
     std::string aStudent_name;
     std::string aStudent_email;
     int last_student_id = 0;
-    std::vector<student> students;
+    std::vector<student> student_list;
+    int student_index;
 
     while(true) {
         std::cout << "Please choose:" << std::endl;
@@ -46,15 +57,37 @@ int main() {
 
         switch (user_selection) {
             case 1: {
-                std::cout << "Enter student's name: " << std::endl;
-                std::cin >> aStudent_name;
-                std::cout << "Enter student's email: " << std::endl;
-                std::cin >> aStudent_email;
-                std::cin.ignore();
-                std::cin.get();
+                    std::cout << "Enter student's name: " << std::endl;
+                    std::cin.ignore();
+                    std::getline(std::cin, aStudent_name);
 
-                student s(last_student_id, aStudent_name, aStudent_email);
-                students.push_back(s);
+                    std::cout << "Enter student's email: " << std::endl;
+                    std::getline(std::cin, aStudent_email);
+
+                    last_student_id ++;
+                    student s(last_student_id, aStudent_name, aStudent_email);
+                    student_list.push_back(s);
+                break;
+            }
+            case 2: {
+                for (int i = 0; i < student_list.size(); i++) {
+                    std::cout << "Student ID: " << student_list[i].student_id << std::endl;
+                    std::cout << "Student Name: " << student_list[i].student_name << std::endl;
+                    std::cout << "Student Email: " << student_list[i].student_email << std::endl;
+                }
+
+                std::cout << "Enter the Student ID of the student you wish to edit: " << std::endl;
+                std::cin.ignore();
+                std::getline(std::cin, student_index);
+                student.modfiy_student(student_index);
+                break;
+            }
+            case 10: {
+                for (int i = 0; i < student_list.size(); i++){
+                    std::cout << "Student ID: " << student_list[i].student_id << std::endl;
+                    std::cout << "Student Name: " << student_list[i].student_name << std::endl;
+                    std::cout << "Student Email: " << student_list[i].student_email << std::endl;
+                }
                 break;
             }
         }
